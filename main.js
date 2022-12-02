60e0126be37a9cbc6fe00c8a4aa41469d2e4df81
DF64bit <96969533+DF64bit@users.noreply.github.com>
2022-12-02 15:08:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 window.onload = function() {
-    window.showOpenFilePicker();
+    // store a reference to our file handle
+let fileHandle;
+
+async function getFile() {
+  // open file picker
+  [fileHandle] = await window.showOpenFilePicker();
+
+  if (fileHandle.kind === 'file') {
+    window.alert("ああ、これはファイルですね。fcbd");
+  } else if (fileHandle.kind === 'directory') {
+    window.alert("ああ、これはディレクトリですね。fcbd");
+  }
+
+}
 }
