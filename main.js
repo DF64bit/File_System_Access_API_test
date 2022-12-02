e06a312d5e33979452f8576fff22c2f2c754dbdd
DF64bit <96969533+DF64bit@users.noreply.github.com>
2022-12-03 08:43:51 +0900

    deleet {}

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var button = document.querySelector(".openfile");
 button.addEventListener("click",
 (e)=>{
-  var [fileHandle] = await window.showOpenFilePicker({});
+  var [fileHandle] = await window.showOpenFilePicker();
   var writable = await fileHandle.createWritable();
   await writable.write(window.prompt("書き込むの入力しろ"));
   await writable.close();
